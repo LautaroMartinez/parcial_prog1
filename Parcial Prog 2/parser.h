e8dda14a9a645d7e30749f40fdb2db6aabba75e8
Lautaro Martinez Larroulet <31736454+LautaroMartinez@users.noreply.github.com>
2022-06-23 20:23:18 -0300

    Borro parcial 2 para subir nuevo

--- Parcial Prog 2/parser.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "LinkedList.h"
-#include "Passenger.h"
-
-void parsePassengerFromText(FILE* pFile , LinkedList* pArrayListPassenger);
-void parsePassengerFromBinary(FILE* pFile , LinkedList* pArrayListPassenger);
